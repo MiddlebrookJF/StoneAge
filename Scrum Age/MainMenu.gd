abb0fd5203002cc65890a835a7e5425e6655a4ae
JSimoneaux <62511650+JSimoneaux@users.noreply.github.com>
2021-10-02 16:31:35 -0400

    fixed it

--- Scrum Age/MainMenu.gd
+++ Scrum Age/MainMenu.gd
@@ -1,25 +1,41 @@
 extends MarginContainer
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-
 func _on_Start_pressed():
-	visible = false
-	get_parent().get_node("Board").show()
-	#get_tree().change_scene("res://Board.tscn")
+	Global.goto_scene("res://Board.tscn")
+
 
 func _on_End_pressed():
 	get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
+
+
+func _on_PlayerOne_text_changed(new_text):
+	Global.playernameone = $VBoxContainer/VBoxContainer/PlayerOne.text
+
+
+func _on_PlayerTwo_text_changed(new_text):
+	Global.playernametwo= 	$VBoxContainer/VBoxContainer/PlayerTwo.text
+
+
+func _on_PlayerThree_text_changed(new_text):
+	Global.playernamethree = $VBoxContainer/VBoxContainer/PlayerThree.text
+
+
+func _on_PlayerFour_text_changed(new_text):
+	Global.playernamefour = $VBoxContainer/VBoxContainer/PlayerFour.text
+	
+
+func _on_PlayerFive_text_changed(new_text):
+	Global.playernamefive = $VBoxContainer/VBoxContainer/PlayerFive.text
+
--- Scrum Age/PlayerMenu.gd
+++ Scrum Age/PlayerMenu.gd
@@ -1,14 +1,12 @@
 extends MarginContainer
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Players/HBoxContainer/PlayerOne").text = Global.playernameone
+	get_node("Players/PlayerOne").text = Global.playernameone
 	get_node("Players/PlayerTwo").text = Global.playernametwo
 	get_node("Players/PlayerThree").text = Global.playernamethree
 	get_node("Players/PlayerFour").text = Global.playernamefour
