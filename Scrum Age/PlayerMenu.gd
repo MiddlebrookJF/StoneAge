a71f4bace02dbf9c6a2afffd08e8e6b1ab345711
JSimoneaux <62511650+JSimoneaux@users.noreply.github.com>
2021-10-12 17:10:47 -0400

    Updated background and player GUI
    I know it looks shitty but at least we have a design concept. Also meeples are bombs because I could not find a meeple sprite small enough to fit.

--- Scrum Age/PlayerMenu.gd
+++ Scrum Age/PlayerMenu.gd
@@ -6,11 +6,11 @@ extends MarginContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Players/PlayerOne").text = Global.playernameone
-	get_node("Players/PlayerTwo").text = Global.playernametwo
-	get_node("Players/PlayerThree").text = Global.playernamethree
-	get_node("Players/PlayerFour").text = Global.playernamefour
-	get_node("Players/PlayerFive").text = Global.playernamefive
+	get_node("Players/HBoxContainer/MarginContainer/NinePatchRect/PlayerOne").text = Global.playernameone
+	get_node("Players/HBoxContainer2/MarginContainer/NinePatchRect/PlayerTwo").text = Global.playernametwo
+	get_node("Players/HBoxContainer3/MarginContainer/NinePatchRect/PlayerThree").text = Global.playernamethree
+	get_node("Players/HBoxContainer4/MarginContainer/NinePatchRect/PlayerFour").text = Global.playernamefour
+	get_node("Players/HBoxContainer5/MarginContainer/NinePatchRect/PlayerFive").text = Global.playernamefive
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
