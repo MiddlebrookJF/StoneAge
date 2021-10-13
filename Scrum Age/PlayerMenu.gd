e73cc07a080c29e6453710e6fc47459c2bb48c90
eyong0 <57815391+eyong0@users.noreply.github.com>
2021-10-12 20:13:46 -0400

    turn indicator setup
    added icons slots next to player names that can be changed in a future user story when turns are changed

--- Scrum Age/PlayerMenu.gd
+++ Scrum Age/PlayerMenu.gd
@@ -6,11 +6,11 @@ extends MarginContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Players/PlayerOne").text = Global.playernameone
-	get_node("Players/PlayerTwo").text = Global.playernametwo
-	get_node("Players/PlayerThree").text = Global.playernamethree
-	get_node("Players/PlayerFour").text = Global.playernamefour
-	get_node("Players/PlayerFive").text = Global.playernamefive
+	get_node("Players/P1 Container/PlayerOne").text = Global.playernameone
+	get_node("Players/P2 Container/PlayerTwo").text = Global.playernametwo
+	get_node("Players/P3 Container/PlayerThree").text = Global.playernamethree
+	get_node("Players/P4 Container/PlayerFour").text = Global.playernamefour
+	get_node("Players/P5 Container/PlayerFive").text = Global.playernamefive
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
