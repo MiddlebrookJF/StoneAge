7adf6e5d79d7aaaf666f479665284fb9e893ed3b
eyong0 <57815391+eyong0@users.noreply.github.com>
2021-10-16 13:31:37 -0400

    fixed player menu issues from merge
    fixed the merge conflicts that came from my branch and johnny's

--- Scrum Age/PlayerMenu.gd
+++ Scrum Age/PlayerMenu.gd
@@ -26,11 +26,11 @@ func _ready():
 		else:
 			turnOrder[first + i] = nameArray[i]
 	
-	get_node("Players/P1 Container/PlayerOne").text = turnOrder[0]
-	get_node("Players/P2 Container/PlayerTwo").text = turnOrder[1]
-	get_node("Players/P3 Container/PlayerThree").text = turnOrder[2]
-	get_node("Players/P4 Container/PlayerFour").text = turnOrder[3]
-	get_node("Players/P5 Container/PlayerFive").text = turnOrder[4]
+	get_node("Players/HBoxContainer/MarginContainer/NinePatchRect/PlayerOne").text = turnOrder[0]
+	get_node("Players/HBoxContainer2/MarginContainer/NinePatchRect/PlayerTwo").text = turnOrder[1]
+	get_node("Players/HBoxContainer3/MarginContainer/NinePatchRect/PlayerThree").text = turnOrder[2]
+	get_node("Players/HBoxContainer4/MarginContainer/NinePatchRect/PlayerFour").text = turnOrder[3]
+	get_node("Players/HBoxContainer5/MarginContainer/NinePatchRect/PlayerFive").text = turnOrder[4]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
