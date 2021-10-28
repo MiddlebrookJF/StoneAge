011e3e318059f4346dc7492d3f45e46d840c9022
Jason Middlebrook <MiddlebrookJF@gmail.com>
2021-10-27 20:32:42 -0400

    Meeple Color User Story
    Meeples turn red because player 1 is the only active player right now, box sprites for empty meeple locations on grids turn white after meeples are removed.

--- Scrum Age/Board.gd
+++ Scrum Age/Board.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-var current_player = 0
+var current_player = 1
 
+const white = Color(1, 1, 1)
 const red = Color(1, 0.262, 0.262)
 const blue = Color(0.608, 0.651, 1)
 const green = Color(0.227, 0.718, 0.377)
@@ -18,7 +19,7 @@ var knight_path = load("res://assets/Meeples/knight_head.png")
 #Get colored textures associated with each player, based on player value calling method (NEEDS TO BE UPDATED)
 func set_meeple_color(texture_path, player):
 	match player:
-		0: return emptySpace
+		0: get_node(texture_path).modulate = white
 		1: get_node(texture_path).modulate = red
 		2: get_node(texture_path).modulate = blue
 		3: get_node(texture_path).modulate = green
@@ -44,19 +45,20 @@ func updateMeepleLabels(label):
 func touch_slot(grid_name, slot):
 	var child = get_node(grid_name)
 	if child.booleanSlotArray[slot-1] == 0:
-			if (Global.meeple_counts[current_player] > 0):	
-				set_meeple_color(grid_name+"/$Slot"+str(slot), current_player); #Set the texture to the player's color
+			if (Global.meeple_counts[current_player] > 0):
+				set_meeple_color(grid_name+"/Slot"+str(slot), current_player); #Set the texture to the player's color
 				child.get_node("Slot"+str(slot)).texture_normal = knight_path
 				child.booleanSlotArray[slot-1] = 1;
 				subtractMeeples(current_player, 1);
 				print(child.booleanSlotArray);
 				print(Global.meeple_counts);
-				$PlayerMenu.updateMeepleLabels();			
+				$PlayerMenu.updateMeepleLabels();
 	else:
 		if child.booleanSlotArray[slot-1] != 0:
 			child.get_node("Slot"+str(slot)).texture_normal = emptySpace;
+			set_meeple_color(grid_name+"/Slot"+str(slot), 0);
 			child.booleanSlotArray[slot-1] = 0;
-			addMeeples(current_player, 1);			
-			print(Global.meeple_counts);		
-			$PlayerMenu.updateMeepleLabels();		
+			addMeeples(current_player, 1);
+			print(Global.meeple_counts);
+			$PlayerMenu.updateMeepleLabels();
 
