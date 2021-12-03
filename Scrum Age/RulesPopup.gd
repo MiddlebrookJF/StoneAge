42f42c8aea4854479387e6b202d19e8b4181d96e
JSimoneaux <62511650+JSimoneaux@users.noreply.github.com>
2021-12-02 20:26:14 -0500

    updated rules a tiny bit

--- Scrum Age/RulesPopup.gd
+++ Scrum Age/RulesPopup.gd
@@ -14,7 +14,7 @@ func close():
 
 
 func show_PointsRules():
-	$DisplayLabel.text = "Points descriptions"
+	$DisplayLabel.text = "Points\n" + "To earn points, you must place your team members, \n" + "or meeples, in the grid slots of each area to earn \n" + "a random number of points for that area. \n" + "Once you have earned enough points from the \n" + "previous areas, you may place meepls in new areas. \n" + "Once you have earned enough testing points, \n" + "you may spend them to collect Story Points."
 
 
 func show_ScrumpleRules():
