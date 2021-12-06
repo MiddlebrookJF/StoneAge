6f82936554b452d21d31499c4f8c83b95512c850
Mellynkor <ricejr2@etsu.edu>
2021-12-05 21:11:56 -0500

    buttons-cleanup

--- Scrum Age/RulesPopup.gd
+++ Scrum Age/RulesPopup.gd
@@ -15,14 +15,3 @@ func close():
 
 func show_PointsRules():
 	$DisplayLabel.text = "Points\n" + "To earn points, you must place your team members, \n" + "or meeples, in the grid slots of each area to earn \n" + "a random number of points for that area. \n" + "Once you have earned enough points from the \n" + "previous areas, you may place meepls in new areas. \n" + "Once you have earned enough testing points, \n" + "you may spend them to collect Story Points."
-
-
-func show_ScrumpleRules():
-	$DisplayLabel.text = "Scrumple descriptions"
-	
-func show_TurnsRules():
-	$DisplayLabel.text = "Turns descriptions"
-	
-func show_ToolsRules():
-	$DisplayLabel.text = "Tools descriptions"			
-		
